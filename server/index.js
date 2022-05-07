bad40d3eb83f29345d0b91e80e32fefdb332ae44
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-05-07 08:21:25 +0100

    mysql connected to node

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require("express");
 
 const app = express();
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "mustafa246",
+  database: "Login",
+});
+
+app.get("/insert", (req, res) => {
+  db.query(
+    "INSERT INTO Login (username, password) VALUES ('hannan', 'mustafa')",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(result);
+    }
+  );
+});
 
 app.listen(3001, () => {
   console.log("server running on port 3001");
